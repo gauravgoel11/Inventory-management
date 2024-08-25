338136caea0066b91a2ac9afdf87c6fe42de5652
Gaurav Goel <130895972+gauravgoel11@users.noreply.github.com>
2024-08-25 16:39:54 +0530

    start

---
+++ src/main/java/com/inventory/management/InventoryManagement.java
@@ -0,0 +1,15 @@
+
+
+package com.inventory.management;
+
+/**
+ *
+ * @author gaura
+ */
+public class InventoryManagement {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+        new NewJFrame().setVisible(true);
+    }
+}
